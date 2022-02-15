bad3b10c4606591290a386b7b6724cb1922997e4
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-15 01:21:11 -0300

    upload

---
+++ Exercicios Lista Sessão 4/ex 21.py
@@ -0,0 +1,6 @@
+# Leia um valor em libra e apresente-o em Kg
+
+print('Digite um valor em libra')
+l = float(input())
+kg = l*0.5
+print(f'O valor em Kg é {kg}')
---
+++ Exercicios Lista Sessão 4/ex 22.py
@@ -0,0 +1,6 @@
+# Leia um valor de comprimento em jardas e apresente-o em convertido em metros
+
+print('Digite um valor de comprimento em jardas')
+j = float(input())
+m = 0.91*j
+print(f'O valor em metros é {m}')
