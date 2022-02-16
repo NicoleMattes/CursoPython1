a9877180dc4caa4014ae111d0a9e7f5b3d02b908
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-15 23:47:29 -0300

    upload

---
+++ Exercicios Lista Sessão 4/ex 26.py
@@ -0,0 +1,6 @@
+# Leia um valor de área em m2 e apresente-o convertido em hectares
+
+print('Digite o valor da área em m2')
+m = float(input())
+h = m*0.0001
+print(f'O valor da área em hectares é {h}')
---
+++ Exercicios Lista Sessão 4/ex 27.py
@@ -0,0 +1,6 @@
+# Leia um valor de área em hectares e apresente-o convertido em m2
+
+print('Digite o valor da área em hectares')
+h = float(input())
+m = h*1000
+print(f'O valor da área convertido para m2 é {m}')
