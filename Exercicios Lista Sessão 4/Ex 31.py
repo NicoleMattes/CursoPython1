fded54a047312be6533acf3437e27915e403c07f
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-21 22:52:19 -0300

    ex

---
+++ Exercicios Lista Sessão 4/Ex 31.py
@@ -0,0 +1,7 @@
+# Leia um número interio e imprima o seu antecessor e o seu sucessor
+
+print('Digite um número inteiro')
+n = int(input())
+a = n-1
+s = n+1
+print(f'O antecessor do {n} é {a} e o sucessor é {s}')
