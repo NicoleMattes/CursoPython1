4a0e1cae452fce7f1bfc6c1f07b0494eea715e95
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-22 20:55:44 -0300

    ex

---
+++ Exercicios Lista Sessão 4/ex 33.py
@@ -0,0 +1,6 @@
+# Leia o tamanho do lado de um quadrado e imprima o valor da sua área
+
+print('Digite o valor do lado de um quadrado')
+l = float(input())
+a = l*l
+print(f'a área do quadrado é {a}')
