85c825fdf0b72be41586afb2e1f6b494c53db6d5
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-23 17:30:20 -0300

    exercícios

---
+++ Exercicios Lista Sessão 4/ex 32.py
@@ -0,0 +1,11 @@
+# leia um número inteiro e imprima a soma do sucessor do seu triplo com o antecessor do seu dobro
+
+print('Digite um número inteiro')
+n = int(input())
+
+r1 = (n*3)+1
+r2 = (n*2)-1
+
+print(f'O Sucessor do triplo é {r1}')
+print(f'O Antecessor do dobro é {r2}')
+
---
+++ Exercicios Lista Sessão 4/ex 34.py
@@ -0,0 +1,8 @@
+# Leia o valor do raio de um círculo, calcule e imprima a área do círculo correspondente.
+
+print('Digite o valor do raio do círculo')
+r = float(input())
+p = float(3.141592)
+a = p*(r*r)
+print(f'A área do circulo é {a}')
+
