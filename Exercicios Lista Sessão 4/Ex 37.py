5bd66dd79d0910d6dd17f075148964e4fa0ef0d6
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-02-23 23:25:38 -0300

    exercício

---
+++ Exercicios Lista Sessão 4/Ex 37.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um valor de um produto e imprima o valor com desconto, sendo o desconto de 12%
+
+print("Digite o valor do produto")
+prod = float(input())
+desc = prod * 0.88
+print(f"O Valor do produto com desconto de é {desc}")
