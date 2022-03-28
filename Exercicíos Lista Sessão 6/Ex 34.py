70fb2e0b9f06b690b5416407a79a681f1fdf131b
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-03-28 01:27:12 -0300

    list 2

--- Exercicíos Lista Sessão 6/Ex 34.py
+++ Exercicíos Lista Sessão 6/Ex 34.py
@@ -3,3 +3,17 @@ Faça um programa que calcule o menor número divisivel por cada um dos números
 Ex: 2520 é o menor número que pode ser dividido por cada um dos números de 1 a 10, sem sobrar resto.
 """
 
+# intervalo = entre 1 a 20
+# multiplo comum
+
+verify = True
+cont = 1
+while verify:
+    if cont % 1 == 0 and cont % 2 == 0 and cont % 3 == 0 and cont % 4 == 0 and cont % 5 == 0 and cont % 6 == 0 \
+            and cont % 7 == 0 and cont % 8 == 0 and cont % 9 == 0 and cont % 10 == 0 and cont % 11 == 0 \
+            and cont % 12 == 0 and cont % 13 == 0 and cont % 14 == 0 and cont % 15 == 0 and cont % 16 == 0 \
+            and cont % 17 == 0 and cont % 18 == 0 and cont % 19 == 0 and cont % 20 == 0:
+        print(f'{cont}')
+        verify = False
+    else:
+        cont = cont + 1
---
+++ Exercicíos Lista Sessão 6/Ex 35.py
@@ -0,0 +1,31 @@
+"""
+Faça um programa que some os números impares contidos em um intervalo definido pelo usuário. O usuário define o valor
+inicial do intervalo e o valor final deste intervalo e o programa deve somar todos os números impares contidos neste
+intervalo. Caso o usuário digite um intervalo inválido (começando por um valor maior do que o valor final) deve ser
+escrito uma mensagem de erro na tela "intervalo invalido" e o programa termina. Exemplo de tela de saida: "Digite o
+valor inicial e valor final: e valor final 10 - Soma dos impares neste intervalo: 21
+"""
+
+# Entrada de dados:
+# Intervalo deve ser definido pelo usuario através das variaveis A e B
+
+a = int(input('Digite o unicio do intervalo numerico\n'))
+b = int(input('Digite o fim do intervalo numerio\n'))
+ip = int()
+soma = int()
+
+# Condição/validação Verificação se os valores digitados são validos
+
+if a >= 0 or b < a:
+    print('Os valores digitados são validos')
+else:
+    print('ERRO\n'
+          'Um ou mais valores digitados não são validos')
+
+# processamento - loop
+
+for i in range(a, b+1):
+    if i%2 == 1:
+        soma = soma + i
+print(f'{soma}')
+
